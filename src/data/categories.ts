13d8133e0e9281046f5fe10ef7ed1616b05de4e8
Juan Simão <juansimao5@gmail.com>
2026-03-03 23:07:48 -0300

    chore: add category mock data

---
+++ src/data/categories.ts
@@ -0,0 +1,48 @@
+// lista categoria para carregar no componente
+
+export const categories = [
+
+  {
+    id: 1,
+     label: 'Tecnologia', 
+      icon: 'src/assets/icons/tecnologia.svg', 
+      active: true 
+  },
+
+  {
+    id: 2,
+     label: 'Supermercado',
+      icon: 'src/assets/icons/supermercados.svg' 
+  },
+
+
+  { 
+    id: 3,
+     label: 'Bebidas',
+      icon: 'src/assets/icons/bebidas.svg' 
+  },
+
+  { 
+    id: 4,
+     label: 'Ferramentas',
+      icon: '/src/assets/icons/ferramentas.svg' 
+  },
+
+  { 
+    id: 5,
+     label: 'Saúde',
+      icon: 'src/assets/icons/cuidados-de-saude.svg' 
+  },
+
+  { 
+    id: 6,
+     label: 'Esportes e Fitness',
+      icon: 'src/assets/icons/corrida.svg' 
+  },
+
+  { 
+    id: 7,
+     label: 'Moda',
+      icon: 'src/assets/icons/moda.svg' 
+  },
+];
