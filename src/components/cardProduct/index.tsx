c3f132fff5c478fd4d01cf0533190327ccc39d3b
Juan Simão <juansimao5@gmail.com>
2026-03-03 22:46:27 -0300

    feat: implement product card component

---
+++ src/components/cardProduct/index.tsx
@@ -0,0 +1,44 @@
+import style from './cardProduct.module.scss'
+import type { Product } from '../../types/product'
+
+
+
+interface CardProductProps {
+  product: Product,
+  onBuy: () => void
+}
+
+
+export function CardProduct({ product, onBuy }: CardProductProps){
+
+  
+
+  return(
+    
+    <article className={style.articleProduct}>
+      <img src={product.photo}  alt={product.productName}  />
+
+      <p className={style.description}>    
+        {product.descriptionShort}
+      </p>
+
+      <div className={style.prices}>
+          <span className={style.oldPrice}>R$ 30,90</span>
+          <strong className={style.currentPrice}>R$ {product.price}</strong>
+      </div>
+
+      <p className={style.payDescription}>
+        ou 2x de R${(product.price / 2).toFixed(2)} sem juros
+      </p>
+
+      <span className={style.freeShipping}>
+        Frete grátis
+      </span>
+
+      <button className={style.buyButton} onClick={onBuy}>
+        COMPRAR
+      </button>
+  </article>
+
+    )
+}
